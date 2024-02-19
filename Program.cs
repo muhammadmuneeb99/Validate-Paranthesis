13000a0c5fa4e630e26a3d3323d2090a7676f0f2
Muhammad Muneeb <32203945+muhammadmuneeb99@users.noreply.github.com>
2024-02-20 01:58:07 +0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+
+bool isValid(string brackets)
+{
+	Stack<char> paranthesis = new();
+	foreach (char c in brackets)
+	{
+		switch (c)
+		{
+			case '{':
+				paranthesis.Push('}');
+				break;
+			case '(':
+				paranthesis.Push(')');
+				break;
+			case '[':
+				paranthesis.Push(']');
+				break;
+			case '<':
+				paranthesis.Push('>');
+				break;
+			case '}':
+			case ')':
+			case ']':
+			case '>':
+				if (paranthesis.Count == 0) return false;
+				if (paranthesis.Pop() != c) return false;
+				break;
+		}
+	}
+	return paranthesis.Count == 0;
+}
+
+Console.WriteLine(isValid("[]{}()"));
+Console.WriteLine(isValid("[}[}()"));
+Console.WriteLine(isValid("[}{](]"));
+Console.WriteLine(isValid("{]{}[)"));
+Console.WriteLine(isValid("{[()]}"));
+Console.ReadLine();
